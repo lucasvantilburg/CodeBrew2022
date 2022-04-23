6f0bd0118fe908424704f1275589b639180c09ab
Lucas Van Tilburg <lucasvantilburg101202@gmail.com>
2022-04-23 14:55:29 +1000

    change port

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
-import node from "@sveltejs/adapter-node";
+import node from "@sveltejs/adapter-node";import preprocess from "svelte-preprocess";
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		//adapter: adapter(),
-		adapter: node({ env: { port: process.env.PORT } }),
+  /** @type {import('@sveltejs/kit').Config} */
+  const config = {
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: preprocess(),
 
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
-	}
-};
+    kit: {
+      // hydrate the <div id="svelte"> element in src/app.html
+      target: "#svelte",
+      adapter: node({ env: { port: process.env.PORT } }),
+    },
+  };
 
-export default config;
+  export default config;
